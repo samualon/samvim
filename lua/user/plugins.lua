c38d8dea2d230518d0448d423eab46aa52464886
Samuel <samuelcoryn@gmail.com>
2022-11-19 23:01:42 +0100

    Personal changes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -41,9 +41,20 @@ packer.init {
 -- Install your plugins here
 return packer.startup(function(use)
   -- My plugins here
+
+
+
+
+
+
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+
+
+
+
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
