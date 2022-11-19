a3895947b93b46318ec2b293d5cd2e0cd358bb9c
Samuel <samuelcoryn@gmail.com>
2022-11-19 22:43:05 +0100

    Personal changes

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -37,10 +37,6 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
--- Insert --
--- Press jk fast to enter
-keymap("i", "jk", "<ESC>", opts)
-
 -- Visual --
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
