3776b0691c09798495845cea9dfa4270e40e5d69
Samuel <samuelcoryn@gmail.com>
2022-11-21 00:12:11 +0100

    Added Treesitter plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -83,6 +83,9 @@ return packer.startup(function(use)
   }
   use "nvim-telescope/telescope-media-files.nvim"
 
+  -- Treesitter
+  use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate",}
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
