d6344246ac48bab1f7d8321cbb5af0614b5c2200
Samuel <samuelcoryn@gmail.com>
2022-11-21 14:31:20 +0100

    Added instructions to fix Treesitter parser compilation.

--- init.lua
+++ init.lua
@@ -14,5 +14,5 @@ require'lspconfig'.r_language_server.setup{}
 require'lspconfig'.sumneko_lua.setup{}
 require('telescope').load_extension('media_files')
 require "user.telescope"
-require "user.treesitter"
 require 'nvim-treesitter.install'.compilers = { "clang" }
+require "user.treesitter"
