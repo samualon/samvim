bc8cf2c1d5ea8fc10d4a7ba65414c2b1f38070b3
samualon <samuelcoryn@gmail.com>
2022-11-21 22:27:00 +0100

    Added and configured autopairs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -89,6 +89,13 @@ return packer.startup(function(use)
   -- Start screen
   use "glepnir/dashboard-nvim"
 
+  -- Autopairs
+  use "windwp/nvim-autopairs"
+
+
+
+
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
